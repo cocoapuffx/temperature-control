601dcbac043ec96b948bc3d6953103ac7232aaf2
cocoapuffx <cocoapuffx@gmail.com>
2020-09-06 17:58:25 -0500

    Change dynamically background image with temperature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [temperatureValue, setTemperatureValue] = useState(20);
+  const [temperatureValue, setTemperatureValue] = useState(15);
   const [temperatureColor, setTemperatureColor] = useState('warm');
     
   const increaseTemperature = () => {
@@ -12,11 +12,21 @@ const App = () => {
 
     setTemperatureValue(newTemperature);
 
+    while (document.body.classList.length > 0) {
+      document.body.classList.remove(document.body.classList.item(0));
+    }
+
     if(newTemperature >= 30){
+      //document.body.style = 'background: #f00a0a;';
+      document.body.classList.add('background-hot');
       setTemperatureColor('hot');
     } else if(newTemperature >= 15){
+      //document.body.style = 'background: #5d567c;';
+      document.body.classList.add('background-warm');
       setTemperatureColor('warm');
     }else{
+      //document.body.style = 'background: #0022c9;';
+      document.body.classList.add('background-cold');
       setTemperatureColor('cold');
     }
   };
@@ -26,16 +36,25 @@ const App = () => {
     if(temperatureValue === 0) return;
 
     const newTemperature = temperatureValue - 1;
-    if(newTemperature > -1){
-      setTemperatureValue(newTemperature);
 
-      if(newTemperature >= 30){
-        setTemperatureColor('hot');
-      } else if(newTemperature >= 15){
-        setTemperatureColor('warm');
-      }else{
-        setTemperatureColor('cold');
-      }
+    setTemperatureValue(newTemperature);
+
+    while (document.body.classList.length > 0) {
+      document.body.classList.remove(document.body.classList.item(0));
+    }
+
+    if(newTemperature >= 30){
+      //document.body.style = 'background: #f00a0a;'; 
+      document.body.classList.add('background-hot');
+      setTemperatureColor('hot');
+    } else if(newTemperature >= 15){
+      //document.body.style = 'background: #5d567c;';
+      document.body.classList.add('background-warm');
+      setTemperatureColor('warm');
+    }else{
+      //document.body.style = 'background: #0022c9;';
+      document.body.classList.add('background-cold');
+      setTemperatureColor('cold');
     }
   };
 
